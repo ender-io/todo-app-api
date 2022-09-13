e65c9f3a9d8be8a67857fd60cb7b47677b35ad6e
Adrian <adrianmarrero18@gmail.com>
2022-09-13 10:05:16 -0400

    relationship added

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,42 +9,16 @@ class Task extends Model
 {
     use HasFactory;
 
-    // /**
-    //  * The attributes that are mass assignable.
-    //  *
-    //  * @var array<int, string>
-    //  */
-    // protected $fillable = [
-    //     'id',
-    //     'name',
-    //     'description',
-    //     'status_id',
-    //     'category_id',
-    //     'user_id',
-    //     'is_starred',
-    //     'icon',
-    //     'color',
-    //     'expires_at',
-    //     'created_at',
-    //     'updated_at',
-    //     'deleted_at',
-    // ];
-
-    // /**
-    //  * The attributes that should be shown in lists.
-    //  *
-    //  * @var array<int, string>
-    //  */
-    // protected $listable = [
-    //     'name',
-    //     'description',
-    //     'status_id',
-    //     'category_id',
-    //     'user_id',
-    //     'is_starred',
-    //     'icon',
-    //     'color',
-    //     'expires_at',
-    // ];
-
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
