f15489de829352561e1a8349b83946def9dd3db2
Adrian <adrianmarrero18@gmail.com>
2022-09-13 16:58:42 -0400

    ajustes produccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 items-center sm:pt-0">
-                    <h1 class="text-gray-600">TO DO API</h1>
+                    <h1 class="text-gray-600">TO-DO API</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
