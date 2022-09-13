232f34b3c0cacbb31311c4a97025b0aceccabbe2
Adrian <adrianmarrero18@gmail.com>
2022-09-13 09:58:33 -0400

    category model created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(4)->create();
 
         $this->call([
             TaskSeeder::class,
             StatusSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
